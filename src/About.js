2b033e603ab12098815be558119c60cedcb6efd6
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2023-10-14 20:27:46 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section">
+      <h2>About Me</h2>
+      <p>I am highly motivated BTech CSE student of VIT Bhopal, with robust academic background </p>
+        <p>and versatile skill-set in programming and advanced technologies. I am currently learning</p>
+       <p> Web development using MernStack and ReactJs, which is taught by Ethus faculties. </p>
+       <p>Connect with me, I am a keen learner and try my best to understand concepts in depth.</p>
+       
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: dinesh.sirvi2021@vitbhopal.ac.in</p>
+      <p>Phone: 9462788221</p>
+      <p>LinkedIn:https://www.linkedin.com/in/dinesh-sirvi-77733b245/</p>
+    </div>
+  );
+};
+
+export default Contact;
